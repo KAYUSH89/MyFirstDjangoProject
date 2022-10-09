fcb3d6b7df279f5de3adf2645c25ae03e5d838bb
Kateryna <usenkok276@gmail.com>
2022-10-09 21:16:25 +0300

    no ch

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -21,8 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
-    re_path(r'^media/(?P.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] \
              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

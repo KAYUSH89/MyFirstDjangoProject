00d21ca96dc2d2cdc4d157a789592cf9dd97139d
Kateryna <usenkok276@gmail.com>
2022-10-13 20:23:58 +0300

    try

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -100,18 +100,18 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 MAX_CONN_AGE = 600
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'test',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #    'NAME': 'test',
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'admin',
+    #    'HOST': '127.0.0.1',
+    #    'PORT': '5432',
+    #}
 
 }
 

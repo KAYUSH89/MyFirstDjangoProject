c954bd3b1fb489979556739535bf214e4034625e
Kateryna <usenkok276@gmail.com>
2022-10-13 19:49:48 +0300

    debug false

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -35,9 +35,9 @@ print(f"SECRET_KEY: {SECRET_KEY}")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-if not IS_HEROKU:
-    DEBUG = True
+DEBUG = False
+#if not IS_HEROKU:
+#    DEBUG = True
 
 if IS_HEROKU:
     ALLOWED_HOSTS = ["*"]
